4f6de0ac54d45119b1e50a5d374953279167eb6b
Serhii Ladonia <ladonya.s@gmail.com>
2018-02-03 20:22:52 +0200

    add parse_equation

--- computor_v1/equation.py
+++ computor_v1/equation.py
@@ -5,3 +5,6 @@ class Equation:
     def __init__(self, equation_str):
         self.equation_str = equation_str
         self.equation = []
+
+    def parse_equation(self):
+        re.findall('([0-9.]*)[ ]*\*[ ]*X\^([012])', self.equation_str)
