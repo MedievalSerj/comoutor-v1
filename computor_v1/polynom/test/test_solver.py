00151c88ac5ce3ff8ad6fb42911c5e3ded7c97df
Serhii Ladonia <ladonya.s@gmail.com>
2018-02-18 21:20:09 +0200

    add test

--- computor_v1/polynom/test/test_solver.py
+++ computor_v1/polynom/test/test_solver.py
@@ -4,5 +4,9 @@ from computor_v1.polynom.solver import Solver
 
 class TestSolver(unittest.TestCase):
 
-    pass
+    def test_filter_powers(self):
+        pass
+
+    def test_reduce(self):
+        pass
 
